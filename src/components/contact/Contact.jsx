722d969d00aac20213c7dc050be4b3b58febbd45
anusha-patel <chandraleka1696@gmail.com>
2023-08-22 16:34:16 +0530

    changed the contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,10 +6,14 @@ const Contact = () => {
         <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full '>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-400'>Contact</p>
-                <p className='py-8'>contact me by only one click</p>
+                <p className='py-8'>contact me by Email</p>
             </div>
 
-            <div className='flex justify-center items-center'>
+            <div>
+                <p className='text-xl'>If you have any questions, feel free to contact me  <a className='text-2xl text-blue-500' href="mailto:anushapatel510@gmail.com"> anushapatel510@gmail.com</a></p> 
+            </div>
+
+            {/* <div className='flex justify-center items-center'>
                 <form action="" className='flex flex-col w-full md:w-1/2 '>
                     <input type="text" name="name"  placeholder=' enter your name' id="" className='py-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
                     <input type="text" name="email" placeholder=' enter your email' id="" className='py-2 my-4 bg-transparent border-2 rounded-md text-white focus:outline-none' />
@@ -17,7 +21,7 @@ const Contact = () => {
 
                     <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
                 </form>
-            </div>
+            </div> */}
         </div>
     </div>
   )
