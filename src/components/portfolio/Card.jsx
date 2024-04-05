8a3c318e9d94d5f07149ab95329c58375296142c
anusha-patel <chandraleka1696@gmail.com>
2024-04-05 11:30:59 +0530

    added e-comm app and change the list to tags

--- src/components/portfolio/Card.jsx
+++ src/components/portfolio/Card.jsx
@@ -3,106 +3,130 @@ import './card.css'
 import project1 from '../../assets/food image.png';
 import project2 from '../../assets/project2.avif';
 import project5 from '../../assets/snake game screenshot.png';
-import project6 from '../../assets/education site image.png';
 import project7 from '../../assets/project7.png'
 import project9 from '../../assets/project9.png';
 import weather from '../../assets/weather_app.png'
 import calorieCheck from '../../assets/calorie-check.png'
+import Ecomm from '../../assets/e-comm.png'
 
 
 
 const Card = () => {
     const width ='60%';
     const height = '60%';
-
-
     const portfolio = [
+
         {
             id: 1,
+            title: 'E-commerce website',
+            src: Ecomm,
+            Tags: (
+                <>
+                   <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JS</li>
+                    <li>REACT</li>
+                    <li>NODE JS</li>
+                    <li>MONGO DB</li>
+                </>
+            ),
+            demo: 'https://e-commerce-front-psi.vercel.app/',
+            code: 'https://github.com/anusha-patel/e-commerce'
+
+        },
+        {
+            id: 2,
             title: 'Resturant website',
             src: project1,
-            lan_1: 'HTML',
-            lan_2: 'CSS',
-            lan_3: 'Javascript',
+            Tags: (
+                <>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JS</li>
+                    
+                </>
+            ),
             demo: 'https://anusha-patel.github.io/food/',
             code: 'https://github.com/anusha-patel/food'
 
         },
         {
-            id: 2,
+            id: 3,
             title: 'real estate',
-            lan_1: 'HTML',
-            lan_2: 'Material UI',
-            lan_3: 'Javascript',
-            lan_4: 'React js',
-            lan_5:'Next js & rapid API',
+            Tags: (
+                <>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JS</li>
+                    <li>REACT</li>
+                    <li>Material UI</li>
+                    <li>Next js </li>
+                    <li>rapid API </li>
+                    
+                </>
+            ),
+           
             src: project9,
             demo: 'https://real-estate-pink-nine.vercel.app/',
             code: 'https://github.com/anusha-patel/real-estate',
 
         },
         {
-            id: 3,
+            id: 4,
             title: 'weather app',
-            lan_1: 'HTML',
-            lan_2: 'CSS',
-            lan_3: 'Javascript',
-            lan_4: 'react',
-            lan_5: 'tailwind, open Api & unsplash Api',
+            Tags: (
+                <>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JS</li>
+                    <li>REACT</li>
+                    <li>tailwind</li>
+                    <li> open Api</li>
+                    <li> unsplash Api</li>
+                </>
+            ),
+           
             src: weather,
             demo: 'https://anusha-patel.github.io/weatherapp_1/',
             code: 'https://github.com/anusha-patel/weatherapp_1',
 
         },
         {
-            id: 4,
+            id: 5,
             src: calorieCheck,
             title: 'calorie-check',
-            lan_1: 'HTML',
-            lan_2: 'Tailwind css',
-            lan_3: 'Javascript',
-            lan_4:'react js',
-            lan_5:'Edamam API',
+            Tags: (
+                <>
+                    <li>HTML</li>
+                    <li>Tailwind css</li>
+                    <li>JS</li>
+                    <li>REACT</li>
+                    <li>Edamam API</li>
+                    
+                </>
+            ),
             demo: 'https://anusha-patel.github.io/calorie-check/',
             code: 'https://github.com/anusha-patel/calorie-check',
         },
         {
-            id: 5,
+            id: 6,
             title: 'Travel website',
-            lan_1: 'HTML',
-            lan_2: 'CSS',
-            lan_3: 'Javascript',
+            Tags: (
+                <>
+                    <li>HTML</li>
+                    <li>Tailwind css</li>
+                    <li>JS</li>
+                    
+                    
+                </>
+            ),
+           
             src: project2,
             demo: 'https://anusha-patel.github.io/travel/',
             code: 'https://github.com/anusha-patel/travel',
 
         },
-        
-
-        // {
-        //     id: 6,
-        //     src: project5,
-        //     title: 'snake Game',
-        //     lan_1: 'HTML',
-        //     lan_2: 'CSS',
-        //     lan_3: 'Javascript',
-        //     demo: 'https://anusha-patel.github.io/snakeGame/',
-        //     code: 'https://github.com/anusha-patel/snakeGame',
-        // },
-       
-        {
-            id: 7,
-            src: project7,
-            title: 'google-keep-clone',
-            lan_1: 'HTML',
-            lan_2: 'CSS',
-            lan_3: 'Javascript',
-            demo: "https://anusha-patel.github.io/google-keep-clone/",
-            code: "https://github.com/anusha-patel/google-keep-clone",
-        },
-        
-       
-
+ 
     ]
     return (
         <div id='portfolio' className='duration-100'>
@@ -114,26 +138,20 @@ const Card = () => {
                         <p className='py-4'>check some of my works</p>
                     </div>
                     <div className="card-container grid sm:grid-cols-1 md:grid-cols-2 gap-6 px-12 sm:px-0">
-                        {portfolio.map(({ id, src, title, demo, code, lan_1, lan_2, lan_3, lan_4, lan_5 }) => (
+                        {portfolio.map(({ id, src, title, demo, code, Tags }) => (
                             <div className="card shadow-md shadow-gray-600" key={id}>
                                 <div className='card-body'>
                                     <div className="title">
                                         <h1>{title}</h1>
                                         <h2 className='ml-3 p-3'>Languages & Tools</h2>
-                                        <ul className=' w-90 ml-10 text-cyan-50 mb-7 ' >
-                                            <li>{lan_1}</li>
-                                            <li>{lan_2}</li>
-                                            <li>{lan_3}</li>
-                                            <li>{lan_4}</li>
-                                            <li>{lan_5}</li>
-
-                                        </ul>
+                                       
+                                        <div className=' w-90 ml-5 text-cyan-50 mb-7'> {Tags}</div>
 
 
 
                                     </div>
 
-                                    <div className="btns">
+                                    <div className="btns relative bottom-0">
                                         <a href={demo} target="_blank" rel="noreferrer noopener"> <button className='demo link-btn duration-200'>demo</button> </a>
                                         <a href={code} target="_blank" rel="noreferrer noopener"> <button className='code link-btn duration-200'>code</button> </a>
                                     </div>
